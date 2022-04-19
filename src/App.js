5e08a52ebd8cf3c5f9db8a883ee8ab156de25ac3
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-04-19 16:06:39 +0300

    Render: calculator to react APP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <h1>this is simple react app</h1>
+      <Calculator />
     </div>
   );
 }
