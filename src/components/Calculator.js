354c1c3ea5ee10437d09a4179363d5b3e833fe6b
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-21 21:26:33 +0300

    Remove: this keyword from callback props

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -90,27 +90,27 @@ export default function Calculator() {
         <input type="text" name="result" id="result" value={result} />
       </div>
       <div id="leftSide">
-        <Button value="AC" buttonName="AC" clickCallback={this.calculateNewData} />
-        <Button value="+/-" buttonName="+/-" clickCallback={this.calculateNewData} />
-        <Button value="%" buttonName="%" clickCallback={this.updateOperator} />
-        <Button value="7" clickCallback={this.updateNextNumber} />
-        <Button value="8" clickCallback={this.updateNextNumber} />
-        <Button value="9" clickCallback={this.updateNextNumber} />
-        <Button value="4" clickCallback={this.updateNextNumber} />
-        <Button value="5" clickCallback={this.updateNextNumber} />
-        <Button value="6" clickCallback={this.updateNextNumber} />
-        <Button value="1" clickCallback={this.updateNextNumber} />
-        <Button value="2" clickCallback={this.updateNextNumber} />
-        <Button value="3" clickCallback={this.updateNextNumber} />
-        <Button value="0" className="zero" clickCallback={this.updateNextNumber} />
-        <Button value="." clickCallback={this.updateNextNumber} />
+        <Button value="AC" buttonName="AC" clickCallback={calculateNewData} />
+        <Button value="+/-" buttonName="+/-" clickCallback={calculateNewData} />
+        <Button value="%" buttonName="%" clickCallback={updateOperator} />
+        <Button value="7" clickCallback={updateNextNumber} />
+        <Button value="8" clickCallback={updateNextNumber} />
+        <Button value="9" clickCallback={updateNextNumber} />
+        <Button value="4" clickCallback={updateNextNumber} />
+        <Button value="5" clickCallback={updateNextNumber} />
+        <Button value="6" clickCallback={updateNextNumber} />
+        <Button value="1" clickCallback={updateNextNumber} />
+        <Button value="2" clickCallback={updateNextNumber} />
+        <Button value="3" clickCallback={updateNextNumber} />
+        <Button value="0" className="zero" clickCallback={updateNextNumber} />
+        <Button value="." clickCallback={updateNextNumber} />
       </div>
       <div id="rightSide">
-        <Button className="divide operator" value="÷" clickCallback={this.updateOperator} />
-        <Button className="multiply operator" value="x" clickCallback={this.updateOperator} />
-        <Button className="minus operator" value="-" clickCallback={this.updateOperator} />
-        <Button className="plus operator" value="+" clickCallback={this.updateOperator} />
-        <Button className="equal operator" value="=" clickCallback={this.calculateNewData} />
+        <Button className="divide operator" value="÷" clickCallback={updateOperator} />
+        <Button className="multiply operator" value="x" clickCallback={updateOperator} />
+        <Button className="minus operator" value="-" clickCallback={updateOperator} />
+        <Button className="plus operator" value="+" clickCallback={updateOperator} />
+        <Button className="equal operator" value="=" clickCallback={calculateNewData} />
       </div>
     </div>
   );
