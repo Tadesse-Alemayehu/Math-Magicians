cdadfce95aa70b1ee9d9a1bf8d07579b587c1165
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-21 21:25:08 +0300

    Remove: render method

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -83,36 +83,37 @@ export default function Calculator() {
     // }));
   }
 
-  render() {
-    const { result } = this.state;
-    return (
-      <div id="Calculator">
-        <div id="result">
-          <input type="text" name="result" id="result" value={result} />
-        </div>
-        <div id="leftSide">
-          <Button value="AC" buttonName="AC" clickCallback={this.calculateNewData} />
-          <Button value="+/-" buttonName="+/-" clickCallback={this.calculateNewData} />
-          <Button value="%" buttonName="%" clickCallback={this.updateOperator} />
-          <Button value="7" clickCallback={this.updateNextNumber} />
-          <Button value="8" clickCallback={this.updateNextNumber} />
-          <Button value="9" clickCallback={this.updateNextNumber} />
-          <Button value="4" clickCallback={this.updateNextNumber} />
-          <Button value="5" clickCallback={this.updateNextNumber} />
-          <Button value="6" clickCallback={this.updateNextNumber} />
-          <Button value="1" clickCallback={this.updateNextNumber} />
-          <Button value="2" clickCallback={this.updateNextNumber} />
-          <Button value="3" clickCallback={this.updateNextNumber} />
-          <Button value="0" className="zero" clickCallback={this.updateNextNumber} />
-          <Button value="." clickCallback={this.updateNextNumber} />
-        </div>
-        <div id="rightSide">
-          <Button className="divide operator" value="÷" clickCallback={this.updateOperator} />
-          <Button className="multiply operator" value="x" clickCallback={this.updateOperator} />
-          <Button className="minus operator" value="-" clickCallback={this.updateOperator} />
-          <Button className="plus operator" value="+" clickCallback={this.updateOperator} />
-          <Button className="equal operator" value="=" clickCallback={this.calculateNewData} />
+
+  return (
+    <div id="Calculator">
+      <div id="result">
+        <input type="text" name="result" id="result" value={result} />
+      </div>
+      <div id="leftSide">
+        <Button value="AC" buttonName="AC" clickCallback={this.calculateNewData} />
+        <Button value="+/-" buttonName="+/-" clickCallback={this.calculateNewData} />
+        <Button value="%" buttonName="%" clickCallback={this.updateOperator} />
+        <Button value="7" clickCallback={this.updateNextNumber} />
+        <Button value="8" clickCallback={this.updateNextNumber} />
+        <Button value="9" clickCallback={this.updateNextNumber} />
+        <Button value="4" clickCallback={this.updateNextNumber} />
+        <Button value="5" clickCallback={this.updateNextNumber} />
+        <Button value="6" clickCallback={this.updateNextNumber} />
+        <Button value="1" clickCallback={this.updateNextNumber} />
+        <Button value="2" clickCallback={this.updateNextNumber} />
+        <Button value="3" clickCallback={this.updateNextNumber} />
+        <Button value="0" className="zero" clickCallback={this.updateNextNumber} />
+        <Button value="." clickCallback={this.updateNextNumber} />
+      </div>
+      <div id="rightSide">
+        <Button className="divide operator" value="÷" clickCallback={this.updateOperator} />
+        <Button className="multiply operator" value="x" clickCallback={this.updateOperator} />
+        <Button className="minus operator" value="-" clickCallback={this.updateOperator} />
+        <Button className="plus operator" value="+" clickCallback={this.updateOperator} />
+        <Button className="equal operator" value="=" clickCallback={this.calculateNewData} />
+      </div>
+    </div>
+  );
 
 
 
-          cpdcpccpcfccfcccsfcfccsfccf
