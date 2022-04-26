6a15ee955ed9e30f9cff0c0202e2b6544073d416
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-26 20:45:51 +0300

    Set intial route components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 export default function Navbar() {
-  [{ name: Home, link: "/" }]
+  // [{ name: Home, link: '/' }];
   return (
     <ul>
       <NavLink to="/">Home</NavLink>
     </ul>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import App from './App';
+import Calculator from './components/Calculator';
+import Home from './components/pages/Home';
+import Quote from './components/pages/Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Calculator" element={<Calculator />} />
+        <Route path="about" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
