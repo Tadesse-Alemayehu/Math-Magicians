de912c17793227bb967d4904997eac076f9e336d
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-04-27 11:30:56 +0300

    Add test for operator buttons

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -9,3 +9,14 @@ test('test calculator for the buttons functionality', () => {
   expect(calculate(passingObject, '+/-'))
     .toEqual({ total: '1', next: '-2', operation: '+' });
 });
+
+test('test operators for the right return', () => {
+  expect(calculate({ ...passingObject, operation: '+' }, '=').total)
+    .toBe('3');
+  expect(calculate({ ...passingObject, operation: '-' }, '').total)
+    .toBe('-1');
+  expect(calculate({ ...passingObject, operation: 'x' }, '=').total)
+    .toBe('2');
+  expect(calculate({ ...passingObject, operation: "÷" }, '=').total)
+    .toBe('0.5');
+});
