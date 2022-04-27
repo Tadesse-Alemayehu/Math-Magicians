8177d1f97e7cb18d4edfa143574db4504f4caa17
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-04-27 12:20:09 +0300

    test operation file 100%

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -6,4 +6,10 @@ test('test operator function for right calculation', () => {
   expect(operator(passingObject.total, passingObject.next, '-')).toBe('-1');
   expect(operator(passingObject.total, passingObject.next, 'x')).toBe('2');
   expect(operator(passingObject.total, passingObject.next, "÷")).toBe('0.5');
+  expect(operator(passingObject.total, passingObject.next, "%")).toBe('1');
+  expect(operator(passingObject.total, 0, "%")).toBe(`Can't find modulo as can't divide by 0.`);
+  expect(operator(passingObject.total, 0, "÷")).toBe(`Can't divide by 0.`);
+  expect(() => {
+    operator(passingObject.total, 0, "unknown")
+  }).toThrow();
 });
