8e63fb7116fb6ed6119a2800f3ff12386787012e
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-27 15:08:40 +0300

    Add test for the Quot component

---
+++ src/components/test/Quote.test.js
@@ -0,0 +1,6 @@
+import renderer from 'react-test-renderer';
+import Quote from '../pages/Quote';
+it('should render Quotes component', () => {
+  const quote = renderer.create(<Quote />).toJSON();
+  expect(quote).toMatchSnapshot();
+})
